92a316266c351d1800fbb8f35950ea55581c0a70
akashsinghal <akashksinghal98@gmail.com>
2016-06-18 10:59:00 +0530

    working on separate views for each agency

---
+++ Volunteer-Connect/ResultViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ResultViewController.swift
+//  Volunteer-Connect
+//
+//  Created by Akash Singhal on 6/18/16.
+//  Copyright © 2016 VolIndustries. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    @IBOutlet weak var AgencyName: UILabel!
+    @IBOutlet weak var hours: UILabel!
+    @IBOutlet weak var days: UILabel!
+    @IBOutlet weak var contact: UILabel!
+    @IBOutlet weak var descriptiontext: UITextView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func sendEmail(sender: UIButton) {
+    }
+    @IBAction func callPhone(sender: UIButton) {
+    }
+    @IBAction func getDirections(sender: UIButton) {
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
